961f15f8ee41b7df689dbf134383d9bcf980e0ba
DingDean <dingkework@hotmail.com>
2017-03-15 20:17:08 +0800

    使用v-for来批量处理文章

--- src/components/Articles.container.vue
+++ src/components/Articles.container.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="articles-container">
     <h1>下面会有文章</h1>
-    <my-article :mdHtml="md | marked"></my-article>
+    <my-article v-for="article in articles" :key="article.id" :mdHtml="article.content | md2Html"></my-article>
   </div>
 </template>
 
@@ -9,18 +9,26 @@
 import MyArticle from './Articles.vue'
 import marked from 'marked'
 
+var md2Html = function (md) {
+  return marked(md, {sanitize: true})
+}
+
 export default {
   name: 'article',
   data () {
     return {
-      md: '### 这是一篇文章'
+      md: '### 这是一篇文章',
+      articles: [
+        {content: '### 这是一个三标题', id: '1'},
+        {content: '~~这段文字会被划掉~~', id: '2'}
+      ]
     }
   },
   components: {
     MyArticle
   },
   filters: {
-    marked
+    md2Html
   }
 }
 </script>
