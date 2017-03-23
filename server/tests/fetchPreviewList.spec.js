0ff957eea3c73a56cd8452cb8ce8e538c522b2e1
DingDean <dingkework@hotmail.com>
2017-03-23 22:51:36 +0800

    fetchPreviewList的scan能获取文章名字的列表

--- server/tests/fetchPreviewList.spec.js
+++
@@ -1,4 +0,0 @@
-const expect = require('exprect.js')
-
-describe('fetchPreviewList获得文章预览列表'function () {
-})
--- server/utils/fetchPreviewList.js
+++ server/utils/fetchPreviewList.js
@@ -4,7 +4,14 @@ module.exports = function (location) {
   var previewList = [];
   var self = this;
 
-  this.get = function () {
-    fs.
+  this.scan = function () {
+    fs.readdir(location, 'utf8', (err, files) => {
+      if (err)
+        console.log('Error')
+      else
+        previewList = files
+    })
   }
+
+  this.get = function () {return previewList;}
 }
