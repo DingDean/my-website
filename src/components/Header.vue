8e493522e256810011d9255b0c5bfe5b754425be
DingDean <dingkework@hotmail.com>
2017-04-12 13:01:17 +0800

    加入网页抬头以及一些想法

---
+++ src/components/Header.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="my-header">
+    <h1>二向箔</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'my-header'
+}
+</script>
+
+<style scoped>
+h1 {
+  text-align: center;
+}
+</style>
