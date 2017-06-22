4e64077cffd16b447dcc936cc8c1c24268040c99
DingDean <dingkework@hotmail.com>
2017-06-22 17:41:27 +0800

    fix connection issue

--- server/server.js
+++ server/server.js
@@ -10,10 +10,10 @@ const mdb = mongoose.connection
 
 handleMongo(mdb)
 
-app.use(compression)
 app.use(require('connect-history-api-fallback')())
 app.use('/static', express.static(path.resolve(__dirname, '../dist/static')))
 app.use('/', express.static(path.resolve(__dirname, '../dist')))
+app.use(compression)
 
 app.get('/', function (req, res) {
   res.sendFile(path.resolve(__dirname, '../dist/index.html'))
