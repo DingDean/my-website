f4dcac213f14e3639c080a9bbb953cf2cdb0ccb3
DingDean <dingkework@hotmail.com>
2017-08-18 10:24:55 +0800

    use async component

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,11 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import MyArticles from '@/components/Articles.container'
-import MyArticle from '@/components/Articles'
-import MyAbout from '@/components/About'
+// import MyArticles from '@/components/Articles.container'
+// import MyArticle from '@/components/Articles'
+// import MyAbout from '@/components/About'
+const MyArticles = () => import('@/components/Articles.container')
+const MyArticle = () => import('@/components/Aritcles')
+const MyAbout = () => import('@/components/About')
 
 Vue.use(Router)
 
