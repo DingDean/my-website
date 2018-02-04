b0d2cbc3502997f0b982d19a1aa8ae238d2cc16e
DingDean <dingkework@hotmail.com>
2018-02-04 17:20:06 +0800

    [NEW] 主页模拟Tmux - 完成移动版

--- src/App.vue
+++ src/App.vue
@@ -1,40 +1,6 @@
 <template>
   <div id="app">
-    <div id="tmux-container">
-
-      <div id="tmux-main">
-        <div class="tmux-pane" id="pane-left">
-          <p class="prompt">/Users/丁科/ >> <span>whoami | cat</span> </p>
-          <div class="stdout">
-            <p>嘿，你好，欢迎来到我的二向箔！</p>
-            <p>我叫丁科，现居杭州，是一名后端(NodeJS)工程师。</p>
-            <p>我惯用JavaScript, Python以及Go。</p>
-            <p>在这里，你可以找到我的书评，技术文章以及一些私人絮语。</p>
-            <p>同时也可以给我发邮件: dingkework@hotmail.com </p>
-            <p>祝你一天愉快！ </p>
-          </div>
-        </div>
-        <div class="tmux-pane" id="pane-right">
-          <p class="prompt">/Users/丁科/Projects >> <span>ls -1G</span></p>
-          <div class="stdout">
-            <ul>
-              <li> > <a href="#"> Blog </a> </li>
-              <li> > <a href="#"> tmux_pm25 </a> </li>
-              <li> > <a href="#"> GitHub </a> </li>
-              <li> > <a href="#"> 豆瓣 </a> </li>
-            </ul>
-          </div>
-        </div>
-      </div>
-
-      <div id="tmux-info">
-        <span id="info-a">[1-二向箔]</span>
-        <span id="info-b">观察 思考 创造</span>
-        <span id="info-c">18/02/04 日 12:22</span>
-      </div>
-
-    </div>
-
+    <router-view></router-view>
   </div>
 </template>
 
@@ -47,54 +13,9 @@ export default {
 @import url("https://fonts.googleapis.com/css?family=Space+Mono")
 body, html
   background-color dark-primary-c
-#app
   font-family 'Space Mono', serif
   -webkit-font-smoothing antialiased
   -moz-osx-font-smoothing grayscale
-  height 100%
-
-  #tmux-container
-    position absolute
-    top 10%
-    left 10%
-    width 80%
-    min-width 1200px
-    height 80%
-    border solid #151212 3px
-    background-color light-primary-c
-    display grid
-    grid-template-rows auto 32px
-
-    #tmux-main
-      height 100%
-      display grid
-      grid-template-columns .5fr .5fr
-      .tmux-pane:first-child
-        border-right dashed divider-c 2px
-      .tmux-pane
-        padding 5px
-        font-size 1.5em
-        .prompt
-          margin-left 10px
-          span
-            color accent-c
-        .stdout
-          margin 10px
-          line-height 1.8em
-
-    #tmux-info
-      width 100%
-      height 32px
-      border solid #151212 1px
-      line-height 32px
-      font-size .8em
-      text-align center
-      #info-a
-        position absolute
-        left 10px
-      #info-b
-        display inline-block
-      #info-c
-        position absolute
-        right 10px
+#app
+  width 100%
 </style>
--- src/views/Index.vue
+++ src/views/Index.vue
@@ -1,37 +1,147 @@
 <template>
-  <div class="ov-container">
-    <post></post>
-    <douban></douban>
+  <div id="tmux-container">
+
+    <div id="tmux-main">
+      <div class="tmux-pane" id="pane-left">
+        <p class="prompt">/Users/丁科/ >> <span>whoami | cat</span> </p>
+        <div class="stdout">
+          <p>嘿，你好，欢迎来到我的二向箔！</p>
+          <p>我叫丁科，现居杭州，是一名后端(NodeJS)工程师。</p>
+          <p>我惯用JavaScript, Python以及Go。</p>
+          <p>在这里，你可以找到我的书评，技术文章以及一些私人絮语。</p>
+          <p>同时也可以给我发邮件: dingkework@hotmail.com </p>
+          <p>祝你一天愉快！ </p>
+        </div>
+      </div>
+      <div class="tmux-pane" id="pane-right">
+        <p class="prompt">/Users/丁科/Projects >> <span>ls -1G</span></p>
+        <div class="stdout">
+          <ul>
+            <li> > <a _rel="noopener" target="_blank" href="https://blog.dingkewz.com"> Blog </a> </li>
+            <li> > <a _rel="noopener" target="_blank" href="https://github.com/DingDean/tmux_pm25"> tmux_pm25 </a> </li>
+            <li> > <a _rel="noopener" target="_blank" href="https://github.com/DingDean/"> GitHub </a> </li>
+            <li> > <a _rel="noopener" target="_blank" href="https://www.douban.com/people/deanacroic/"> 豆瓣 </a> </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+
+    <div id="tmux-info">
+      <span id="info-a">[1-二向箔]</span>
+      <span id="info-b">观察 思考 创造</span>
+      <span id="info-c">{{localDate}}</span>
+    </div>
+
   </div>
 </template>
 
 <script>
-import Douban from '../components/IndexPage/ov.douban'
-// import Github from './ov.github'
-// import Musics from './ov.music'
-// import German from './ov.deutsch'
-import Post from '../components/IndexPage/ov.post'
-
 export default {
-  components: {
-    Douban,
-    // Github,
-    // Musics,
-    // German,
-    Post
+  computed: {
+    localDate () {
+      let now = new Date()
+      let minutes = now.getMinutes()
+      if (minutes < 10) {
+        minutes = `0${minutes}`
+      }
+      return now.toLocaleDateString() + ' ' + now.getHours() + ':' + minutes
+    }
   }
 }
 </script>
 
 <style lang="stylus">
-.ov-container
-  padding 10px 30px
-.i-ov-title
-  line-height 24px
-  margin-left 15px
-  color white
-  font-size 20pt
-.i-ov-icon
-  color white
-  float left
+@media (min-width: 600px)
+  #tmux-container
+    position absolute
+    top 10%
+    left 10%
+    width 80%
+    min-width 1200px
+    height 80%
+    min-height 500px
+    border solid #151212 3px
+    background-color light-primary-c
+    display grid
+    grid-template-rows auto 32px
+
+  #tmux-main
+    height 100%
+    display grid
+    grid-template-columns .5fr .5fr
+  .tmux-pane:first-child
+    border-right dashed divider-c 2px
+  .tmux-pane
+    padding 5px
+    font-size 1.5em
+    .prompt
+      margin-left 10px
+      span
+        color accent-c
+    .stdout
+      margin 10px
+      line-height 1.8em
+
+  #tmux-info
+    width 100%
+    height 32px
+    border solid #151212 1px
+    line-height 32px
+    font-size .8em
+    text-align center
+    #info-a
+      position absolute
+      left 10px
+    #info-b
+      display inline-block
+    #info-c
+      position absolute
+      right 10px
+
+@media (max-width: 600px)
+  #tmux-container
+    position absolute
+    top 5%
+    left 5%
+    width 90%
+    height 90%
+    border solid #151212 3px
+    background-color light-primary-c
+    display grid
+    grid-template-rows auto 32px
+
+  #tmux-main
+    display grid
+    grid-template-rows .5fr .5fr
+    grid-template-columns 1fr
+  .tmux-pane:first-child
+    border-bottom dashed divider-c 2px
+  .tmux-pane
+    padding 5px
+    font-size .8em
+  .prompt
+    margin-left 10px
+    span
+      color accent-c
+  .stdout
+    margin 10px
+    line-height 3em
+
+  #tmux-info
+    width 100%
+    height 32px
+    border solid #151212 1px
+    line-height 32px
+    font-size .5em
+    #info-a
+      float left
+      margin 0 10px
+    #info-c
+      float right
+      margin 0 10px
+@media (max-height: 640px)
+  .tmux-pane
+    font-size .3em
+  .stdout
+    line-height 2em
 </style>
