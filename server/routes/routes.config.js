306918588434c40f1dd2e501c27aec9383ae259c
DingDean <dingkework@hotmail.com>
2018-02-24 12:24:39 +0800

    [MODIFY] remove some routes temperarily

--- server/routes/routes.config.js
+++ server/routes/routes.config.js
@@ -1,6 +1,6 @@
-const path = require('path')
+// const path = require('path')
 
 module.exports = [
-  {path: '/api/douban', module: path.resolve(__dirname, './api.douban.js')},
-  {path: '/api/blog', module: path.resolve(__dirname, './api.post.js')}
+  // {path: '/api/douban', module: path.resolve(__dirname, './api.douban.js')},
+  // {path: '/api/blog', module: path.resolve(__dirname, './api.post.js')}
 ]
