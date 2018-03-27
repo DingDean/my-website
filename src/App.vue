b728a9669b5df38c16375a8ae3d8cc46c38ca47b
DingDean <dingkework@hotmail.com>
2018-03-27 12:13:32 +0800

    index.vue: use local fonts instead of google font

--- src/App.vue
+++ src/App.vue
@@ -10,10 +10,15 @@ export default {
 </script>
 
 <style lang="stylus">
-@import url("https://fonts.googleapis.com/css?family=Space+Mono")
+@font-face
+    font-family: Chancery;
+    src: url(./assets/fonts/chancery/apple-chancery-webfont.eot);
+    src: local("Apple Chancery"),url(./assets/fonts/chancery/apple-chancery-webfont.eot?#iefix) format("embedded-opentype"),url(./assets/fonts/chancery/apple-chancery-webfont.woff2) format("woff2"),url(./assets/fonts/chancery/apple-chancery-webfont.woff) format("woff"),url(./assets/fonts/chancery/apple-chancery-webfont.ttf) format("truetype"),url(./assets/fonts/chancery/apple-chancery-webfont.svg#apple-chancery) format("svg");
+    font-weight: lighter;
+    font-style: normal
 body, html
   background-color dark-primary-c
-  font-family 'Space Mono', serif
+  font-family serif
   -webkit-font-smoothing antialiased
   -moz-osx-font-smoothing grayscale
 #app
